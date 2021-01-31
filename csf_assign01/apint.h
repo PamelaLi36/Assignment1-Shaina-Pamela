a76a2ed23170ffaac544e7f80040fc571dd976fb
Live session user <sgabala2@jh.edu>
2021-01-31 19:55:38 +0000

    Work on project so far

---
+++ csf_assign01/apint.h
@@ -0,0 +1,50 @@
+/*
+ * Arbitrary-precision integer data type
+ */
+
+#ifndef APINT_H
+#define APINT_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Representation: the data field is a little-endian bitstring ---
+ * data[0] is bits 0..63, data[1] is bits 64..127, etc.
+ */
+typedef struct {
+  uint32_t len; /*number of data values */
+  bool isNegative; /* true if negative, false if positive */
+  uint64_t *data; /* data values */
+} ApInt;
+
+/* Constructors and destructors */
+ApInt *apint_create_from_u64(uint64_t val);
+ApInt *apint_create_from_hex(const char *hex);
+void apint_destroy(ApInt *ap);
+
+/* Operations */
+int apint_is_zero(const ApInt *ap);
+int apint_is_negative(const ApInt *ap);
+uint64_t apint_get_bits(const ApInt *ap, unsigned n);
+int apint_highest_bit_set(const ApInt *ap);
+char *apint_format_as_hex(const ApInt *ap);
+ApInt *apint_negate(const ApInt *ap);
+ApInt *apint_add(const ApInt *a, const ApInt *b);
+ApInt *apint_sub(const ApInt *a, const ApInt *b);
+int apint_compare(const ApInt *left, const ApInt *right);
+
+/*helper functions*/  
+uint64_t addVal(uint64_t a, uint64_t b);
+uint64_t subVal(uint64_t a, uint64_t b);
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APINT_H */
